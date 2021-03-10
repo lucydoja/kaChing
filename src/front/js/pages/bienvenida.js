c3ffc5132a4ea3d535f3cc9c8bbb6d4f8fa32f85
MILENA1988 <milip19@gmail.com>
2021-03-10 21:53:18 +0000

    add phrase

--- src/front/js/pages/bienvenida.js
+++ src/front/js/pages/bienvenida.js
@@ -49,7 +49,7 @@ export const Bienvenida = () => {
 						<h5 className="card-title">Second step</h5>
 						<p className="card-text">Don´t have an account? Register One!</p>
 						<p className="card-text">
-							<small className="text-muted">Last updated 3 mins ago</small>
+							<small className="text-muted">Believe in your dreams</small>
 						</p>
 					</div>
 				</div>
