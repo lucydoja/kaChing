0d2eee53d93fceccf12cc6c4e0be17565504297d
andresborn <andres.born@gmail.com>
2021-03-10 23:26:25 +0000

    Added register and login route with JWT

--- migrations/versions/11c622af0f80_.py
+++ migrations/versions/11c622af0f80_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 9dfa028729bb
+Revision ID: 11c622af0f80
 Revises: 
-Create Date: 2021-03-10 21:31:43.861577
+Create Date: 2021-03-10 23:11:25.160666
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '9dfa028729bb'
+revision = '11c622af0f80'
 down_revision = None
 branch_labels = None
 depends_on = None
--- src/api/routes.py
+++ src/api/routes.py
@@ -12,11 +12,6 @@ api = Blueprint('api', __name__)
 
 ### Routes ###
 
-# Test
-@api.route('/test', methods=["GET"])
-def test():
-    return jsonify({"msg": "test"}), 200
-
 # Register User
 @api.route('/register', methods=["POST"])
 def create_user():
@@ -28,6 +23,13 @@ def create_user():
     security_question = request.json["security_question"]
     security_answer = request.json["security_answer"]
 
+    if not (email and password and first_name and last_name and security_question and security_answer):
+        return jsonify({"error": "Invalid"}), 400
+    
+    check_email = User.query.filter_by(email=email).first()
+    if check_email:
+        return jsonify({"error": "Email already exists"})
+
     hashed_password = generate_password_hash(password)
     hashed_security_answer = generate_password_hash(security_answer)
 
@@ -37,3 +39,31 @@ def create_user():
     db.session.commit()
 
     return jsonify(new_user.serialize()), 200
+
+# Login User
+@api.route('/login', methods=["POST"])
+def login():
+    if request.method == "POST":
+        
+        email = request.json["email"]
+        password = request.json["password"]
+
+        # Validate
+        if not email:
+            return jsonify({"error": "em"}), 400
+        if not password:
+            return jsonify({"error": "pas"}), 400
+
+        user = User.query.filter_by(email=email).first()
+
+        if not user:
+            return jsonify({"error": "usr"}), 400
+        
+        if not check_password_hash(user.password, password):
+            return jsonify({"error": "pashas"}), 400
+        
+        # Create Token
+        access_token = create_access_token(identity=email)
+
+        return jsonify({"access_token": access_token}), 200
+
