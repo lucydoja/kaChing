b737d11974faefea8159c30febb585109d81c897
gaby-oop <gabysanz1505@gmail.com>
2021-03-11 04:27:09 +0000

    creando cambios para enviar la info

--- src/front/js/pages/perfil.js
+++ src/front/js/pages/perfil.js
@@ -1,10 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 
 export const Perfil = () => {
 	const { store, actions } = useContext(Context);
+	const [total, setTotal] = useState(0);
+	const [periodicity, setPeriodicity] = useState("");
+	const [email, setEmail] = useState("");
+	const [name, setName] = useState("");
+	const [lastname, setLastname] = useState("");
 
+	const handleSubmit = e => {
+		e.preventDefault();
+
+		if (periodicity === "Periodicity") {
+			alert("Please select periodicity");
+		}
+
+		const data = {
+			total: total,
+			periodicity: periodicity,
+			email: email,
+			name: name,
+			lastname: lastname
+		};
+	};
 	return (
 		<div className="container mb-2">
 			<div className="row">
@@ -14,14 +34,21 @@ export const Perfil = () => {
 			</div>
 			<hr />
 			<div className="d-flex justify-content-center">
-				<form className="rounded shadow p-2" style={{ width: "550px" }}>
+				<form className="rounded shadow p-2" style={{ width: "550px" }} onSubmit={e => handleSubmit(e)}>
 					<div className="form-row">
 						<div className="col-md d-flex">
 							<label>Total Income </label>
-							<input type="number" min="0" className="form-control" placeholder="$1000" />{" "}
+							<input
+								type="number"
+								min="0"
+								className="form-control"
+								placeholder="$1000"
+								required
+								onChange={e => setTotal(e.target.value)}
+							/>
 						</div>
 						<div className="col-md">
-							<select className="custom-select">
+							<select className="custom-select" required onChange={e => setPeriodicity(e.target.value)}>
 								<option selected disabled>
 									Periodicity
 								</option>
@@ -35,24 +62,45 @@ export const Perfil = () => {
 					<div className="form-row my-2">
 						<div className="col-md">
 							<label>Email</label>
-							<input type="email" className="form-control" placeholder="@gmail.com" />
+							<input
+								type="email"
+								className="form-control"
+								placeholder="@gmail.com"
+								required
+								onChange={e => setEmail(e.target.value)}
+							/>
 						</div>
 					</div>
 					<div className="form-row my-2">
 						<div className="col-md">
 							<label>Name</label>
-							<input type="text" className="form-control" placeholder="Name" />
+							<input
+								type="text"
+								className="form-control"
+								placeholder="Name"
+								required
+								onChange={e => setName(e.target.value)}
+							/>
 						</div>
 					</div>
 					<div className="form-row my-2">
 						<div className="col-md">
 							<label>Last Name</label>
-							<input type="text" className="form-control" placeholder="Last Name" />
+							<input
+								type="text"
+								className="form-control"
+								placeholder="Last Name"
+								required
+								onChange={e => setLastname(e.target.value)}
+							/>
 						</div>
 					</div>
 					<div className="submit-row d-flex justify-content-center my-2">
-						<button type="submit" className="btn btn-primary">
+						<button type="submit" className="btn btn-primary mr-1">
 							Accept
+						</button>
+						<button type="reset" className="btn btn-danger ml-1">
+							Cancel
 						</button>
 					</div>
 				</form>
