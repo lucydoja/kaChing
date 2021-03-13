ae556a895b4030030a1b2889d2d9c696e5fb80db
MILENA1988 <milip19@gmail.com>
2021-03-13 04:47:14 +0000

    structure4faqs

--- src/front/js/pages/faqs.js
+++ src/front/js/pages/faqs.js
@@ -2,6 +2,9 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 import Table from "react-bootstrap/Table";
+import ListGroup from "react-bootstrap/ListGroup";
+import Image from "react-bootstrap/Image";
+import { Card, Container, Row, Col } from "react-bootstrap";
 
 export const Faqs = () => {
 	const { store, actions } = useContext(Context);
@@ -15,42 +18,55 @@ export const Faqs = () => {
 					<span style={{ fontWeight: 400 }} />
 				</h3>
 			</div>
-			<Table striped bordered hover variant="dark">
-				<thead>
-					<tr>
-						<th>#</th>
-						<th>Question</th>
-						<th>Answer</th>
-						<th />
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>1</td>
-						<td>Why kaChing?</td>
-						<td>
-							It is a very simple application with which you will be able to know what percentage of your
-							money is spent on food, transportation, cabs, medical, entertainment or home, among other
-							categories.
-						</td>
-						<td>@mdo</td>
-					</tr>
-					<tr>
-						<td>2</td>
-						<td>I forgot my kaChing password. How can I reset it?</td>
-						<td>
-							Select how you want to recover your password and click Next. (answer your security question
-							and reset your password )
-						</td>
-						<td>@fat</td>
-					</tr>
-					<tr>
-						<td>3</td>
-						<td colSpan="2">Larry the Bird</td>
-						<td>@twitter</td>
-					</tr>
-				</tbody>
-			</Table>
+			<ListGroup>
+				<ListGroup.Item>
+					Why kaChing? Because is a very simple application with which you will be able to know what
+					percentage of your money is spent on food, transportation, cabs, medical, entertainment or home,
+					among other
+				</ListGroup.Item>
+				<ListGroup.Item variant="primary">
+					I forgot my password, how do I gain access to my account? Don´t worry and be happy =) Select how you
+					want to recover your password and click Next. (answer your security question and reset your password
+					)
+				</ListGroup.Item>
+				<ListGroup.Item variant="secondary">
+					Do I need a balance to pay and have access katChing? No You don´t need to have funds in your PayPal
+					balance to use cheKing
+				</ListGroup.Item>
+				<ListGroup.Item variant="success">Do I need to verify my identity? </ListGroup.Item>
+				<ListGroup.Item variant="danger">How do I update my personal details? </ListGroup.Item>
+				<ListGroup.Item variant="warning">
+					I had a kaChing account in the past. How can I reactivate it?{" "}
+				</ListGroup.Item>
+				<ListGroup.Item variant="info">How do I close my katChing account? </ListGroup.Item>
+				<ListGroup.Item variant="light">
+					My email and password are both correct but I still can´t log in. Why? Simply go to the Login page
+					and click the Forgot your password? link.{" "}
+				</ListGroup.Item>
+				<ListGroup.Item variant="dark">
+					Where can I use kaChing? From anywhere, as long as you have a device and internet access.
+				</ListGroup.Item>
+			</ListGroup>
+			<Container>
+				<Row>
+					<Col xs={6} md={4}>
+						<Image
+							src="https://browser-scarlet-bird-9mgxxue5.ws-us03.gitpod.io/workspace/Proyecto-Final-4Geeks/src/front/img/logo.jpg"
+							rounded
+						/>
+					</Col>
+					<Col xs={6} md={4}>
+						<Image
+							src="https://i.pinimg.com/564x/04/94/cf/0494cff4277231581994a2e363f52dd2.jpg"
+							roundedCircle
+						/>
+					</Col>
+					<Col xs={6} md={4}>
+						<Image src="http://www.tencanal10.tv/wp-content/uploads/2018/04/Untitled-1-8.jpg" thumbnail />
+					</Col>
+				</Row>
+			</Container>
+
 			<div className="posicionFooter" />
 		</div>
 	);
